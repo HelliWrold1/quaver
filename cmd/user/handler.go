f1fe430179e5f04d9cecbd6dabdc229335cc0da3
HelliWrold <albert_yun@qq.com>
2023-02-21 21:16:28 +0800

    user.handler差一个UserInfo的实现

--- cmd/user/handler.go
+++ cmd/user/handler.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"github.com/HelliWrold1/quaver/cmd/user/pack"
 	"github.com/HelliWrold1/quaver/cmd/user/service"
 	user "github.com/HelliWrold1/quaver/kitex_gen/user"
+	"github.com/HelliWrold1/quaver/pkg/errno"
 )
 
 // UserServiceImpl implements the last service interface defined in the IDL.
@@ -11,7 +13,6 @@ type UserServiceImpl struct{}
 
 // UserRegister implements the UserServiceImpl interface.
 func (s *UserServiceImpl) UserRegister(ctx context.Context, req *user.RegisterReq) (resp *user.RegisterResp, err error) {
-	// TODO: Your code here...
 	err = service.NewCreateUserService(ctx).CreateUser(req)
 	if err != nil {
 		return nil, err
@@ -20,10 +21,12 @@ func (s *UserServiceImpl) UserRegister(ctx context.Context, req *user.RegisterRe
 }
 
 // UserLogin implements the UserServiceImpl interface.
-func (s *UserServiceImpl) UserLogin(ctx context.Context, req *user.LoginReq) (resp *user.LoginResp, err error) {
-	// TODO: Your code here...
-
-	return
+func (s *UserServiceImpl) UserLogin(ctx context.Context, req *user.LoginReq) (*user.LoginResp, error) {
+	id, err := service.NewLoginUserService(ctx).LoginUser(req)
+	if err != nil {
+		return pack.BuildLoginFailResp(), errno.AuthorizationFailedErr
+	}
+	return pack.BuildLoginSuccessResp(id), nil
 }
 
 // UserInfo implements the UserServiceImpl interface.
--- cmd/user/pack/resp.go
+++ cmd/user/pack/resp.go
@@ -1,5 +1,26 @@
 package pack
 
-func BuildResp() {
+import (
+	"github.com/HelliWrold1/quaver/kitex_gen/user"
+	"github.com/HelliWrold1/quaver/pkg/errno"
+)
 
+func BuildLoginSuccessResp(id int64) *user.LoginResp {
+	return &user.LoginResp{
+		StatusResp: &user.StatusResp{
+			StatusCode: int32(errno.Success.ErrCode),
+			StatusMsg:  &errno.Success.ErrMsg,
+		},
+		UserId: id,
+	}
+}
+
+func BuildLoginFailResp() (req *user.LoginResp) {
+	return &user.LoginResp{
+		StatusResp: &user.StatusResp{
+			StatusCode: int32(errno.AuthorizationFailedErr.ErrCode),
+			StatusMsg:  &errno.AuthorizationFailedErr.ErrMsg,
+		},
+		UserId: 0,
+	}
 }
